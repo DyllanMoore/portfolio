77f09fb68df76d5e3610d7fcf9e986a1ed18ae1f
Dyllan Moore <dyllantmoore@gmail.com>
2022-06-10 12:26:56 -0500

    condensed project function

--- src/App.js
+++ src/App.js
@@ -6,36 +6,6 @@ import { Skills } from './components/skills';
 import { Projects } from './components/projects';
 import { Contact } from './components/contact';
 
-const projectList = [{
-  projectName: "Ecommerce Storefront",
-  projectImage: "#",
-  projectImageAlt: "Ecommerce Storefront Image",
-  projectDescription: "A storefront that allows users to navigate a carousel of different items for sale, add them to their cart, remove them, and checkout. ",
-  projectLinkGithub: "https://github.com/DyllanMoore/OK-Coders-Store-Challenge"
-},
-{
-  projectName: "Weather App",
-  projectImage: "#",
-  projectImageAlt: "Weather App Image",
-  projectDescription: "An app that utilizes a weather REST API to give the weather for various locations.",
-  projectLinkGithub: "https://github.com/DyllanMoore/OKCoders-Browser-Challenge"
-},
-{
-  projectName: "Daily Homepage",
-  projectImage: "#",
-  projectImageAlt: "Daily Homepage Image",
-  projectDescription: "An inspirational homepage that allows the user to see a quote of the day, the current weather, add goals for the day, and flip through cute puppy pictures.",
-  projectLinkGithub: "https://github.com/DyllanMoore/inspirational-homepage"
-},
-{
-  projectName: "Pokemon App",
-  projectImage: "#",
-  projectImageAlt: "Pokemon App Image",
-  projectDescription: "An app that utilizes the Pokemon Pokidex API to allow the user to search for pokemon.",
-  projectLinkGithub: "https://github.com/DyllanMoore/react-pokemon-app"
-},
-];
-
 const contactList = {
   gitHub: "https://github.com/DyllanMoore",
   linkdin: "https://www.linkedin.com/in/dyllan-ellisor-9880a0229/"
@@ -49,34 +19,7 @@ function App() {
       <Resume />
       <About />
       <Skills />
-      <Projects
-        projectName={projectList[0].projectName}
-        projectImage={projectList[0].projectImage}
-        projectImageAlt={projectList[0].projectImageAlt}
-        projectDescription={projectList[0].projectDescription}
-        projectLinkGithub={projectList[0].projectLinkGithub}
-      />
-      <Projects
-        projectName={projectList[1].projectName}
-        projectImage={projectList[1].projectImage}
-        projectImageAlt={projectList[1].projectImageAlt}
-        projectDescription={projectList[1].projectDescription}
-        projectLinkGithub={projectList[1].projectLinkGithub}
-      />
-      <Projects
-        projectName={projectList[2].projectName}
-        projectImage={projectList[2].projectImage}
-        projectImageAlt={projectList[2].projectImageAlt}
-        projectDescription={projectList[2].projectDescription}
-        projectLinkGithub={projectList[2].projectLinkGithub}
-      />
-      <Projects
-        projectName={projectList[3].projectName}
-        projectImage={projectList[3].projectImage}
-        projectImageAlt={projectList[3].projectImageAlt}
-        projectDescription={projectList[3].projectDescription}
-        projectLinkGithub={projectList[3].projectLinkGithub}
-      />
+      <Projects />
       <Contact
         gitHub={contactList.gitHub}
         linkdin={contactList.linkdin}
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,16 +1,57 @@
 export function Projects(props) {
-
+    const projectList = [{
+        projectName: "Ecommerce Storefront",
+        projectImage: "#",
+        projectImageAlt: "Ecommerce Storefront Image",
+        projectDescription: "A storefront that allows users to navigate a carousel of different items for sale, add them to their cart, remove them, and checkout. ",
+        projectLinkGithub: "https://github.com/DyllanMoore/OK-Coders-Store-Challenge"
+    },
+    {
+        projectName: "Weather App",
+        projectImage: "#",
+        projectImageAlt: "Weather App Image",
+        projectDescription: "An app that utilizes a weather REST API to give the weather for various locations.",
+        projectLinkGithub: "https://github.com/DyllanMoore/OKCoders-Browser-Challenge"
+    },
+    {
+        projectName: "Daily Homepage",
+        projectImage: "#",
+        projectImageAlt: "Daily Homepage Image",
+        projectDescription: "An inspirational homepage that allows the user to see a quote of the day, the current weather, add goals for the day, and flip through cute puppy pictures.",
+        projectLinkGithub: "https://github.com/DyllanMoore/inspirational-homepage"
+    },
+    {
+        projectName: "Pokemon App",
+        projectImage: "#",
+        projectImageAlt: "Pokemon App Image",
+        projectDescription: "An app that utilizes the Pokemon Pokidex API to allow the user to search for pokemon.",
+        projectLinkGithub: "https://github.com/DyllanMoore/react-pokemon-app"
+    },
+    ];
 
     return (
-        <div cllassName="ProjectsContainer" id="Projects">
-            <h4>
-                {props.projectName}
-            </h4>
-            <img src={props.projectImage} alt={props.projectImageAlt} />
-            <p>
-                {props.projectDescription}
-            </p>
-            <a href={props.projectLinkGithub} target="_blank">View on GitHub</a>
-        </div>
+        projectList.map((project) => (
+            <div cllassName="ProjectsContainer" id="Projects">
+                <h4>
+                    {project.projectName}
+                </h4>
+                <img src={project.projectImage} alt={props.projectImageAlt} />
+                <p>
+                    {project.projectDescription}
+                </p>
+                <a href={project.projectLinkGithub} target="_blank">View on GitHub</a>
+            </div>
+        )
+        )
+        // <div cllassName="ProjectsContainer" id="Projects">
+        //     <h4>
+        //         {props.projectName}
+        //     </h4>
+        //     <img src={props.projectImage} alt={props.projectImageAlt} />
+        //     <p>
+        //         {props.projectDescription}
+        //     </p>
+        //     <a href={props.projectLinkGithub} target="_blank">View on GitHub</a>
+        // </div>
     )
 }
