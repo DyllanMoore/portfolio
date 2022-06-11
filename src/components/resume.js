8c2b3bc6bb8f060f27b06664eac5e00c4e3a8c82
Dyllan Moore <dyllantmoore@gmail.com>
2022-06-11 14:25:08 -0500

    fixed resume opening on same tab

--- src/components/resume.js
+++ src/components/resume.js
@@ -4,7 +4,7 @@ export function Resume() {
             <h1>
                 Dyllan Ellisor
             </h1>
-            <a class="ResumeLink" href="https://docs.google.com/document/d/1TnO6cjZagSTR_Oa-OyH7DcY3vge7i4jMsfMAPY9O9Ns/edit?usp=sharing">
+            <a class="ResumeLink" href="https://docs.google.com/document/d/1TnO6cjZagSTR_Oa-OyH7DcY3vge7i4jMsfMAPY9O9Ns/edit?usp=sharing" target="_blank">
                 See Resume
             </a>
         </div>
